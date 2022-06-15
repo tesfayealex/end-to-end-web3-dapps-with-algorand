034e8d874109fe76448235ccfbbafd92af492e59
tesfayealex <tesfayealemayehu27@gmail.com>
2022-06-15 23:45:13 +0300

    frontend dapp initiated and added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Consume from './components/Consume';
+
+function App() {
+  return (
+    <div className="App">
+      <Consume/>
+    </div>
+  );
+}
+
+export default App;
