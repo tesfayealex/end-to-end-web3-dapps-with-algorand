febaca4a9065735bebc0ad0576bbd0fac701295b
tesfayealex <tesfayealemayehu27@gmail.com>
2022-06-17 23:44:21 +0300

    added some urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import IndexAPI
+from .views import IndexAPI, create_new_nft, get_all_user_a_wallet, hello_world ,connect_to_algo
 
 urlpatterns = [
-    path('', IndexAPI.as_view())
+    path('', IndexAPI.as_view()),
+    path('func', hello_world),
+    path('connect', connect_to_algo),
+    path('user', get_all_user_a_wallet),
+    path('create_nft', create_new_nft)
 ]
