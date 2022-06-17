ec658f9a74556dc599d0a63ba3cad736a7c9a79e
tesfayealex <tesfayealemayehu27@gmail.com>
2022-06-17 23:44:46 +0300

    added user profile model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField(max_length=500, blank=True)
+    private_key = models.TextField(max_length=500 , blank=True)
+    certificate_id = models.TextField(max_length=500 , blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
